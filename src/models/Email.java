973aaa2a11ace38b2af52e4fc5bee068ed343c99
unknown <nestor.amichetti@gmail.com>
2019-02-18 19:53:46 -0300

    Revert "Cambios agregados."
    This reverts commit fcab7374b8c5625701490ce1a15132682491ee1e.

---
+++ src/models/Email.java
@@ -0,0 +1,15 @@
+package models;
+
+import java.sql.Blob;
+import java.sql.SQLException;
+
+import data.FactoryConnection;
+
+public class Test {
+
+
+	public static void main(String[] args) {
+	
+		
+	}
+}
